24e42ba13b3d4e2e44d87d04b58463086f6fc6a9
mohammad RaoofNia <mraoofnia@gmail.com>
2018-05-06 07:59:07 +0430

    Update index.php
    some improvisations on how it looks, but it needs more improvisations.

--- index.php
+++ index.php
@@ -3,6 +3,30 @@
 <head>
 	<title>Directories</title>
 </head>
+
+<style type="text/css">
+	p{
+		max-width: 40%;
+		background-color: cyan;
+		border-color: blue;
+		border-style: solid;
+		border-bottom-color: black;
+		border-bottom-style: solid;	
+		padding: 8px;
+		margin:0;
+	}
+
+	br{
+		margin:0;
+		padding:0;
+		height: 0;
+	}
+
+	body{
+		padding-top: 10%;
+	}
+</style>
+
 <body>
 	<center>
 	<?php
@@ -13,7 +37,7 @@ if (is_dir($dir)){
   if ($dh = opendir($dir)){
     while (($file = readdir($dh)) !== false){
     	if(strpos($file,'.')==0 && $file!='.' && $file!='..')
-      echo  "<a href=../".$file.">".$file ."</a><br>";
+      echo  "<p><a href=../".$file.">".$file ."</a></p>";
     }
     closedir($dh);
   }
